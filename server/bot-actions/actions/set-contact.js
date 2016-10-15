da39454e0f62db7e4520dadb554c9cd7ca931d4d
Gabriel Sabillon <gaboelnuevo@gmail.com>
2016-10-15 13:00:38 -0600

    updated setContact

--- server/bot-actions/actions/set-contact.js
+++ server/bot-actions/actions/set-contact.js
@@ -5,13 +5,14 @@ module.exports = function(request) {
   var entities = request.entities;
 
   return new Promise(function(resolve, reject) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
     if (entities.contact && entities.contact[0]) {
       context.contact = entities.contact[0].value;
+      context.missingContact = false;
     } else {
       context.missingContact = true;
-      delete context.contact;
+      if (context.contact) {
+        delete context.contact;
+      }
     }
     return resolve(context);
   });
