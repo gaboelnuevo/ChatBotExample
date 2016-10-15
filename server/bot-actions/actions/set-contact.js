2c295dec5863184fef0846a58e42030d30e9feb5
Gabriel Sabillon <gaboelnuevo@gmail.com>
2016-10-15 12:33:47 -0600

    setContact test

--- server/bot-actions/actions/set-contact.js
+++ server/bot-actions/actions/set-contact.js
@@ -1,6 +1,14 @@
 'use strict';
 
-module.exports = function(context, entities) {
-  console.log('set contact triggered');
-  console.log(entities);
+module.exports = function(request) {
+  var context = _ref.context;
+  var entities = _ref.entities;
+
+  return new Promise(function(resolve, reject) {
+	  // Here should go the api call, e.g.:
+	  // context.forecast = apiCall(context.loc)
+    console.log('set contact triggered');
+    console.log(entities);
+    return resolve(context);
+  });
 };
